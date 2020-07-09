0ab5cc63e60aeb5b389f1778c02efa8be523c393
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-07-09 13:59:32 -0400

    data renders on the client side on the page both image and title

--- main.js
+++ main.js
@@ -1,55 +1,41 @@
-// require("dotenv").config();
-
 const API_KEY = "ff27e3d379a647e0b68a8adb4f8abfcf";
-//process.env.NEWS_API_KEY;
 
 const US_BUSINESS_BASE_URL =
   "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=";
 
 const BASE_URL = "http://newsapi.org/v2/everything?q=";
 
-const US_BILLIONAIRES_BASE_URL = `${BASE_URL}billionaires&from=2020-07-01&sortBy=publishedAt&apiKey=`;
+const US_BILLIONAIRES_BASE_URL = `${BASE_URL}billionaires&pageSize=50&sortBy=publishedAt&apiKey=`;
 
-const US_INNOVATION_BASE_URL = `${BASE_URL}innovation&from=2020-07-01&sortBy=publishedAt&apiKey=`;
+const US_INNOVATION_BASE_URL = `${BASE_URL}innovation&pageSize=50&sortBy=publishedAt&apiKey=`;
 
-const US_LEADERSHIP_BASE_URL = `${BASE_URL}leadership&from=2020-07-01&sortBy=publishedAt&apiKey=`;
+const US_LEADERSHIP_BASE_URL = `${BASE_URL}leadership&pageSize=50&sortBy=publishedAt&apiKey=`;
 
-const US_RAISING_MONEY_BASE_URL = `${BASE_URL}raising-money&from=2020-07-01&sortBy=publishedAt&apiKey=`;
+const US_RAISING_MONEY_BASE_URL = `${BASE_URL}raising-money&pageSize=50&sortBy=publishedAt&apiKey=`;
 
-const US_SMALL_BUSINESS_BASE_URL = `${BASE_URL}small-business&from=2020-07-01&sortBy=publishedAt&apiKey=`;
+const US_SMALL_BUSINESS_BASE_URL = `${BASE_URL}small-business&pageSize=50&sortBy=publishedAt&apiKey=`;
 
-const US_LIFESTYLE_BASE_URL = `${BASE_URL}lifestyle&&from=2020-07-01&sortBy=publishedAt&apiKey=`;
-
-// console.log(`${US_RAISING_MONEY_BASE_URL}${API_KEY}`);
+const US_LIFESTYLE_BASE_URL = `${BASE_URL}lifestyle&pageSize=50&sortBy=publishedAt&apiKey=`;
 
 //----------------------------------------
 
-const newDiv = document.querySelectorAll("Results");
+const newsList = document.querySelector(".results");
 
-window.onload = function () {
-  getNews;
-};
+window.addEventListener("load", async () => {
+  try {
+    let response = await axios.get(`${US_BILLIONAIRES_BASE_URL}${API_KEY}`);
+    console.log("Successfully pulled");
+    console.log("Total Results: ", response.data.totalResults);
+    console.log(response.data.articles.length);
 
-const getNews = async () => {
-  await axios
-    .get(`${US_RAISING_MONEY_BASE_URL}${API_KEY}`)
-    .then(function (response) {
-      console.log("success");
-      console.log(response.data.totalResults);
+    for (let i = 0; i < response.data.articles.length; i += 1) {
+      image = response.data.articles[i].urlToImage;
 
-      const articles = response.data.articles.map((article, index) => {
-        return article;
-      });
-      console.log(articles);
-      // newDiv.innerHTML = `<div id="newsresults" key="index">
-      //   ${articles.title}
-      //   </div>`;
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {});
-};
-getNews();
+      articleTitle = response.data.articles[i].title;
 
-//---------------------------------
+      newsList.innerHTML += `<div><img src=${image} alt="news image"><br>${articleTitle}</div>`;
+    }
+  } catch (error) {
+    console.log("error");
+  }
+});

4e74c407fbf004485a90a1c7fb02353879c5ddbb
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-07-09 23:25:48 -0400

    pagination is functioning fully leveraged Tyler Potts YouTube video to solve

--- main.js
+++ main.js
@@ -19,32 +19,77 @@ const US_LIFESTYLE_BASE_URL = `${BASE_URL}lifestyle&pageSize=50&sortBy=published
 
 //----------------------------------------
 
-const newsList = document.querySelector(".results");
-const articleQty = document.querySelector(".article-numbers");
-const nextPage = document.querySelector(".next-page");
-
-
 window.addEventListener("load", async () => {
   try {
     let response = await axios.get(`${US_BILLIONAIRES_BASE_URL}${API_KEY}`);
     console.log("Successfully pulled");
     console.log("Total Results: ", response.data.totalResults);
-    console.log(response.data.articles.length);
-    numberOfArticles = response.data.articles.length
 
+    const newsList = document.querySelector(".results");
 
-    for (let i = 0; i < 10; i += 1) {
+    const articleArrays = response.data.articles;
 
-    // for (let i = 0; i < response.data.articles.length; i += 1) {
-      image = response.data.articles[i].urlToImage;
-      articleTitle = response.data.articles[i].title;
-      
-      newsList.innerHTML += `<div class="result"><img class="article-image" src=${image} alt="news image" placeholder="../style/BN.png"><br><p class="article-title">${articleTitle}</p></div>`;
+    const articleArray = document.getElementById("results");
+    const pagination_element = document.getElementById("pagination");
 
+    let current_page = 1;
+    let articlesshown = 10;
+
+    //Split data into lists and renders initial page
+    function DisplayList(items, wrapper, articlesshown_per_page, page) {
+      newsList.innerHTML = "";
+      page -= 1;
+
+      let start = articlesshown_per_page * page;
+      let end = start + articlesshown_per_page;
+      let paginatedItems = items.slice(start, end);
+
+      for (let i = 0; i < paginatedItems.length; i += 1) {
+        let image = paginatedItems[i].urlToImage;
+        let articleTitle = paginatedItems[i].title;
+        let articleSource = paginatedItems[i].source.name;
+
+        newsList.innerHTML += `<div class="result">
+        <img class="article-image" src=${image} alt="news image" placeholder="../style/BN.png">
+        <br>
+        <p class="article-title">${articleTitle}</p>
+        <br>
+        <p class="article-title">${articleSource}</p>
+        </div>`;
+      }
     }
-    
-    articleQty.innerHTML = `<div class="article-numbers"><h4 class="article-title">Showing 10 of ${numberOfArticles}</h4></div>`;
 
+    //setup pagination buttons
+    function SetupPagination(items, wrapper, articlesshown_per_page) {
+      wrapper.innerHTML = "";
+      let page_count = Math.ceil(items.length / articlesshown_per_page);
+      for (let i = 1; i < page_count + 1; i++) {
+        let btn = PaginationButton(i, items);
+        wrapper.appendChild(btn);
+      }
+    }
+
+    // show current list
+    function PaginationButton(page, items) {
+      let button = document.createElement("button");
+      button.innerText = page;
+
+      if (current_page == page) button.classList.add("active");
+
+      button.addEventListener("click", function () {
+        current_page = page;
+        DisplayList(items, articleArray, articlesshown, current_page);
+
+        let current_btn = document.querySelector(".pagenumbers button.active");
+        current_btn.classList.remove("active");
+
+        button.classList.add("active");
+      });
+      return button;
+    }
+
+    DisplayList(articleArrays, articleArray, articlesshown, current_page);
+    SetupPagination(articleArrays, pagination_element, articlesshown);
   } catch (error) {
     console.log("error");
   }
